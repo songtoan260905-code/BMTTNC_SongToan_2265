5fba71e262d909d58f7906b774f57ed5132efdad
unknown <songtoan260905@gmail.com>
2026-03-09 16:27:41 +0700

    [add] lab02

--- lab02/Cipher/Vigenere/vigenere_cipher.py
+++ lab02/Cipher/Vigenere/vigenere_cipher.py
@@ -1,6 +1,8 @@
 class VigenereCipher:
     def __init__(self):
         pass
+    
+    
     def vigenere_encrypt(self, plain_text, key):
         encrypted_text = ""
         key_index = 0
@@ -10,13 +12,14 @@ class VigenereCipher:
                 if char.isupper():
                     encrypted_text += chr((ord(char) - ord('A') + key_shift) % 26 + ord('A'))
                 else:
-                    encrypted_text += chr((ord(char) - ord('A') + key_shift) % 26 +ord('a'))
+                    encrypted_text += chr((ord(char) - ord('a') + key_shift) % 26 +ord('a'))
                 key_index += 1
             else:
                 encrypted_text += char
                 
-            return encrypted_text
-        def vigenere_decrypt(self, encrypted_text, key):
+        return encrypted_text
+    
+    def vigenere_decrypt(self, encrypted_text, key):
             decrypted_text = ""
             key_index = 0
             for char in encrypted_text:
@@ -26,8 +29,14 @@ class VigenereCipher:
                         decrypted_text += chr((ord(char) - ord('A') - key_shift) % 26 + ord('A'))
                     else:
                         decrypted_text += chr((ord(char) - ord('a') - key_shift) % 26 + ord('a'))
-                        key_index += char
+                        
+                    key_index += 1
                 else:
                     decrypted_text +=char
-                return decrypted_text
+                    
+            return decrypted_text
+        
+        
+        
+        
                         
